b58dee18bee11a78374bb98277834f99be145af2
lalatina11 <rahmadancandra@gmail.com>
2025-02-10 10:34:59 +0700

    Feat: Profile Card and Left Menu Building

--- src/components/PageComponents/LeftMenu.tsx
+++ src/components/PageComponents/LeftMenu.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import ProfileCard from "./ProfileCard";
 
-const LeftMenu = () => {
-  return (
-    <div>LeftMenu</div>
-  )
+interface LeftMenuProps {
+  type: "home" | "profile";
 }
 
-export default LeftMenu
+const LeftMenu = (props: LeftMenuProps) => {
+  const { type } = props;
+  return (
+    <div className="flex flex-col gap-6">
+      {type === "home" ? <ProfileCard /> : null}
+      <div className="bg-black rounded-lg shadow-md text-sm text-zinc-500 flex flex-col gap-2">
+        
+      </div>
+    </div>
+  );
+};
+
+export default LeftMenu;

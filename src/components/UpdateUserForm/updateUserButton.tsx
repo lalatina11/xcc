6c1997c199bfa48546f4c9b1ae3ff3974fb32a0c
lalatina11 <rahmadancandra@gmail.com>
2025-02-25 10:37:06 +0700

    Validate: validate update user's form using useFormStatus()

---
+++ src/components/UpdateUserForm/updateUserButton.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+const updateUserButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      disabled={pending}
+      className="w-[90%] bg-blue-500 p-2 rounded-md disabled:opacity-50"
+      type="submit"
+    >
+      {!pending ? "Update Profile" : "Updating Profile"}
+    </button>
+  );
+};
+
+export default updateUserButton;

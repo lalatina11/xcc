8e9651f3380d687294777e62658cdb532e0489e7
lalatina11 <rahmadancandra@gmail.com>
2025-02-25 10:38:27 +0700

    Style: styling cursor button when update user form is on pending

--- src/components/UpdateUserForm/updateUserButton.tsx
+++ src/components/UpdateUserForm/updateUserButton.tsx
@@ -7,7 +7,7 @@ const updateUserButton = () => {
   return (
     <button
       disabled={pending}
-      className="w-[90%] bg-blue-500 p-2 rounded-md disabled:opacity-50"
+      className="w-[90%] bg-blue-500 p-2 rounded-md disabled:opacity-50 disabled:cursor-not-allowed"
       type="submit"
     >
       {!pending ? "Update Profile" : "Updating Profile"}

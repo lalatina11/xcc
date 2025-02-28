3f69ce0f549297806a5373a26fffd239e828e6ec
lalatina11 <rahmadancandra@gmail.com>
2025-02-28 11:05:52 +0700

    style: decrease opacity when form action is pending

--- src/components/CenterMenu/AddPostButton.tsx
+++ src/components/CenterMenu/AddPostButton.tsx
@@ -5,7 +5,7 @@ import { IoSend } from "react-icons/io5";
 const AddPostButton = () => {
     const {pending} = useFormStatus()
   return (
-    <button disabled={pending} className="disabled:cursor-not-allowed" type="submit">
+    <button disabled={pending} className="disabled:cursor-not-allowed disabled:opacity-50" type="submit">
       <IoSend className="h-6 w-6" />
     </button>
   );
